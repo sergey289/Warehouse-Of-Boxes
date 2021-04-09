e16970ba1f17df3ee6e57fb00d24040ff4760d22
sergey289 <johnny289v@gmail.com>
2021-04-09 19:34:34 +0300

    height_Y-added methods CompareTo,ToString, CheckAllowable_Quantity

--- BoxesLogic/Data/height_Y.cs
+++ BoxesLogic/Data/height_Y.cs
@@ -6,7 +6,49 @@ using System.Threading.Tasks;
 
 namespace BoxesLogic.Data
 {
-    class height_Y
+    class height_Y:IComparable<height_Y>
     {
+
+        public double y;
+
+        public int quantity = 0;
+
+        public int MaxQuantity = 20;
+
+        public height_Y(double y, int quantity)
+        {
+
+            this.y = y;
+
+            this.quantity = quantity;
+
+        }
+
+        public int CompareTo(height_Y other)
+        {
+            return y.CompareTo(other.y);
+        }
+
+        public override string ToString()
+        {
+
+            return $" height={y}, quantity={quantity}\n";
+        }
+
+        public bool CheckAllowable_Quantity()
+        {
+
+            if (quantity <= 0)
+            {
+                return true;
+
+            }
+
+            return false;
+
+
+        }
+
+
     }
 }
