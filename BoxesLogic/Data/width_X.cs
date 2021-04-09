f27f22f1fe469620c66ec1ad9053656afff7d289
sergey289 <johnny289v@gmail.com>
2021-04-09 19:42:31 +0300

    width_X-created data structure  LinkedList

--- BoxesLogic/Data/width_X.cs
+++ BoxesLogic/Data/width_X.cs
@@ -8,7 +8,25 @@ namespace BoxesLogic.Data
 {
     class width_X
     {
-        
+
+        public double x { get; set; }
+
+        public LinkedList<height_Y> y_dataList = new LinkedList<height_Y>();
+
+        public width_X(double x, double y, int quantity)
+        {
+
+            this.x = x;
+
+            height_Y height = new height_Y(y, quantity);
+
+            y_dataList.AddFirst(height);
+
+
+        }
+
+
+
 
 
 
