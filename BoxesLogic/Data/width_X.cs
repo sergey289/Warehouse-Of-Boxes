e34b1203bc5d933d5d2c063530f413fa0caa0e87
sergey289 <johnny289v@gmail.com>
2021-04-09 19:47:34 +0300

    width_X-added methods  CompareTo,ToString, IFexistY

--- BoxesLogic/Data/width_X.cs
+++ BoxesLogic/Data/width_X.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BoxesLogic.Data
 {
-    class width_X
+    class width_X:IComparable<width_X>
     {
 
         public double x { get; set; }
@@ -25,7 +25,62 @@ namespace BoxesLogic.Data
 
         }
 
+        public int CompareTo(width_X other)
+        {
+            return x.CompareTo(other.x);
+        }
 
+        public override string ToString()
+        {
+            StringBuilder Alldata = new StringBuilder();
+
+
+            foreach (var ydata in y_dataList)
+            {
+                if (ydata.quantity != 0)
+                {
+                    Alldata.Append($"width={x,3} " + $"{ydata,5} " + "\n");
+
+                }
+
+            }
+
+            return Alldata.ToString();
+        }
+
+        public void IFexistY(height_Y data, out height_Y foundData)
+        {
+
+            foundData = default;
+
+            foreach (var vr in y_dataList)
+            {
+
+                if (vr.y == data.y)
+                {
+                    foundData = vr;
+
+
+                }
+
+
+            }
+
+
+        }
+
+        public void FindBestMatch_of_Y(height_Y searchValue, out height_Y cloossetBigger)
+        {
+
+            cloossetBigger = default;
+
+            height_Y temp = y_dataList.OrderBy(i => i.y)
+
+            .Where(i => i.y >= searchValue.y).FirstOrDefault();
+
+            cloossetBigger = temp;
+
+        }
 
 
 
