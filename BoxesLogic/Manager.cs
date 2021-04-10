78007477d108f80e598345d78ad95a21467b0db4
sergey289 <johnny289v@gmail.com>
2021-04-10 09:09:08 +0300

    manager-added method ShowDatabyCriterians

--- BoxesLogic/Manager.cs
+++ BoxesLogic/Manager.cs
@@ -55,6 +55,69 @@ namespace BoxesLogic
         }
 
 
+        public void ShowDatabyCriteriarians(double x, double y)
+        {
+            width_X Xdata = new width_X(x, y, default);
+
+            width_X existing = Tree.Find(Xdata);
+
+
+            if (existing == null) // if not founnd Xdata
+            {
+
+                try
+                {
+
+                    throw new Exception("ERROR:Box with such parameters does not exist");
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+
+                }
+
+            }
+            else // if Xdata exist
+            {
+                height_Y newYdata = new height_Y(y, default);
+
+                height_Y yRoot;
+
+                existing.IFexistY(newYdata, out yRoot);
+
+                try
+                {
+
+                    if (yRoot != null) // if Y_data exist
+                    {
+                        Console.Write("\n");
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine($"width={existing.x}" + "" + yRoot);
+                        Console.ResetColor();
+
+                    }
+                    else // if Y_data not exist
+                    {
+
+                        throw new Exception("ERROR:Box with such parameters does not exist");
+
+                    }
+
+
+                }
+                catch (Exception e)
+                {
+
+                    Console.WriteLine(e.Message);
+
+                }
+
+            }
+
+        }
+
+
         public int AddNewQuantity(int currenntQuantity, int newQuantity, int maxQuantity)
         {
 
