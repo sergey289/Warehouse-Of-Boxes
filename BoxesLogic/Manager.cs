d27f119ce7fde26bce4fa853c8f57f6ebc1f9691
sergey289 <johnny289v@gmail.com>
2021-04-10 11:17:03 +0300

    manager-added method FindBestMatchForGift_lavel_A

--- BoxesLogic/Manager.cs
+++ BoxesLogic/Manager.cs
@@ -134,5 +134,108 @@ namespace BoxesLogic
 
         }
 
+        ///////////////////
+        ///
+
+        static int counter = 0;
+        public void FindBestMatchForGift_lavel_A(double x, double y)
+        {
+
+            width_X Xdata = new width_X(x, y, default);
+
+            width_X FoundMatch_of_X;
+
+            Tree.FindBestMatch(Xdata, Tree.Root, out FoundMatch_of_X);
+
+
+            if (FoundMatch_of_X == null) // if not founnd Xdata
+            {
+
+                try
+                {
+
+                    throw new Exception("Not found match");
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+
+                }
+
+            }
+            else // if Xdata exist
+            {
+                height_Y newYdata = new height_Y(y, default);
+
+                height_Y FoundMatch_of_Y;
+
+                FoundMatch_of_X.FindBestMatch_of_Y(newYdata, out FoundMatch_of_Y);
+
+                try
+                {
+
+                    if (FoundMatch_of_Y != null) // if Y_data exist
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("THE Best match that has been found !\n");
+                        Console.ResetColor();
+                        Console.WriteLine("--------------------------------------");
+
+                        Console.WriteLine($"width={FoundMatch_of_X.x}" + "" + FoundMatch_of_Y + "\n");
+
+                        FoundMatch_of_Y.quantity = FoundMatch_of_Y.quantity - 1;
+
+                        if (FoundMatch_of_Y.CheckAllowable_Quantity()) // checking the quantity if it is acceptable
+                        {
+                            Console.WriteLine("the current number of boxes has reached zero!");
+
+                            FoundMatch_of_X.y_dataList.Remove(FoundMatch_of_Y);
+
+                            if (FoundMatch_of_X.y_dataList == null)
+                            {
+
+                                Tree.Remove(FoundMatch_of_X);
+
+                            }
+
+                            Console.WriteLine("this object has been removed from the system!");
+
+                        }
+
+                    }
+
+                    else // if Y_data not exist
+                    {
+                        width_X Xdata2 = new width_X(x + 0.01, y, default);
+
+                        counter++;
+
+                        if (counter > 100)
+                        {
+                            counter = 0;
+
+                            throw new Exception("Not found match");
+
+                        }
+
+                        FindBestMatchForGift_lavel_A(Xdata2.x, y);
+
+                    }
+
+
+                }
+                catch (Exception e)
+                {
+
+                    Console.WriteLine(e.Message);
+
+                }
+
+            }
+
+
+        }
+
     }
 }
