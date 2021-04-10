d30cb7000aa60f5247cd0855e3e7dd13b694a2a8
sergey289 <johnny289v@gmail.com>
2021-04-10 12:07:20 +0300

    manager added methods FindBestMatchForGift_lavel_B,ShowOptions

--- BoxesLogic/Manager.cs
+++ BoxesLogic/Manager.cs
@@ -237,5 +237,139 @@ namespace BoxesLogic
 
         }
 
+        List<width_X> Alloptions = new List<width_X>(); //all options on request, focusing on the quantity
+
+        List<height_Y> ListOf_heights = new List<height_Y>();
+
+        static double originalValueOF_Y;
+
+        static double originalValueOF_X;
+
+        static int flag = 0;
+
+        public void FindBestMatchForGift_lavel_B(double x, double y, double quan)
+
+        {
+            flag++;
+
+            if (flag == 1)
+            {
+                originalValueOF_Y = y;
+                originalValueOF_X = x;
+
+            }
+
+            width_X Xdata = new width_X(x, y, default);
+
+            width_X FoundMatch_of_X;
+
+            Tree.FindBestMatch(Xdata, Tree.Root, out FoundMatch_of_X);
+
+
+            if (FoundMatch_of_X == null || FoundMatch_of_X.x > originalValueOF_X + (originalValueOF_X * 0.4)) // if not founnd Xdata
+            {
+
+
+                if (Alloptions.Count != 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("THE Best match that has been found !\n");
+                    Console.ResetColor();
+                    Console.WriteLine("--------------------------------------");
+                    ShowOptions();
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Not found match");
+                    Console.ReadLine();
+                }
+
+
+            }
+            else // if Xdata exist
+            {
+
+
+                height_Y newYdata = new height_Y(y, default);
+
+                height_Y FoundMatch_of_Y;
+
+                FoundMatch_of_X.FindBestMatch_of_Y(newYdata, out FoundMatch_of_Y);
+
+
+                if (FoundMatch_of_Y != null) // if Y_data exist
+                {
+
+
+                    if (FoundMatch_of_Y.quantity >= quan)//checking  if the best match contains the required quantity
+                    {
+                        width_X newda = new width_X(FoundMatch_of_X.x, FoundMatch_of_Y.y, FoundMatch_of_Y.quantity);
+
+                        Alloptions.Add(newda);
+
+                        ListOf_heights.Add(FoundMatch_of_Y);
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("THE Best match that has been found !\n");
+                        Console.ResetColor();
+                        Console.WriteLine("--------------------------------------");
+
+                        ShowOptions();
+
+                        return;
+
+                    }
+
+
+                    if (FoundMatch_of_Y.quantity >= quan * 0.4) //checking if  quantity bigger than 40% of request
+                    {
+
+                        width_X newda = new width_X(FoundMatch_of_X.x, FoundMatch_of_Y.y, FoundMatch_of_Y.quantity);
+
+                        Alloptions.Add(newda);
+
+                        ListOf_heights.Add(FoundMatch_of_Y);
+
+                        FindBestMatchForGift_lavel_B(x, FoundMatch_of_Y.y + 0.1, quan);
+
+                    }
+                    else
+                    {
+                        FindBestMatchForGift_lavel_B(x, FoundMatch_of_Y.y + 0.1, quan);
+
+                    }
+
+
+                }
+                else // if Y_data not found
+                {
+
+
+                    FindBestMatchForGift_lavel_B(FoundMatch_of_X.x + 0.01, originalValueOF_Y, quan);
+
+                }
+
+
+            }
+
+        }
+
+        public void ShowOptions()
+        {
+            int counter = 1;
+
+            foreach (var i in Alloptions)
+            {
+
+                Console.WriteLine($"{counter++}) " + i, 10);
+
+
+
+            }
+
+
+
+        }    //output of all options on request, focusing on the quantity
+
     }
 }
